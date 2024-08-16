b9927350812ffed634cf8cfe2a8e61755d5cfaba
James Smith <kaleburdev@gmail.com>
2024-08-15 21:54:24 -0400

    Write a test to ensure that clicking the apply link directs user to correct URL

--- CodeYouApplyTests/UnitTest1.cs
+++ CodeYouApplyTests/UnitTest1.cs
@@ -7,8 +7,8 @@ namespace CodeYouApplyTests
     public class Tests
     {
         private IWebDriver _driver;
-        private string homepageUri = "https://code-you.org";
-        private string applyUriPath = "/apply/";
+        private readonly string homepageUri = "https://code-you.org";
+        private readonly string applyUriPath = "/apply/";
 
         [SetUp]
         public void Setup()
@@ -17,17 +17,20 @@ namespace CodeYouApplyTests
         }
 
         [Test]
-        public void Test1()
+        public void FormSubmission_DisplaysErrorWithSpecificText_WhenBlankFormSubmitted()
         {
             var expectedAlertText = "The form is not complete and has not " +
                 "been submitted yet. There are 28 problems with your submission.";
             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
 
-            _driver.Navigate().GoToUrl(homepageUri);
+            NavigateToHomePage();
             var applyLink = _driver.FindElement(By.XPath("//li[@id='menu-item-44']//a[normalize-space()='Apply']"));
             applyLink.Click();
 
             var submitButton = _driver.FindElement(By.XPath("//input[@id='submit_button']"));
+
+            // Simply using submitButton.Click() kept giving "click intercepted" errors
+            // Clicking via JavaScript works just fine, though
             IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)_driver;
             javaScriptExecutor.ExecuteScript("arguments[0].click();", submitButton);
 
@@ -38,6 +41,16 @@ namespace CodeYouApplyTests
             alert.Dismiss();
 
             Assert.That(expectedAlertText, Is.EqualTo(alertText));
+        }
+
+        [Test]
+        public void HomepageApplyLink_RedirectsToCorrectUri_WhenClicked()
+        {
+            var expectedUri = homepageUri + applyUriPath;
+
+            NavigateToHomePage();
+
+            Assert.That(expectedUri, Is.EqualTo(_driver.Url));
         }
 
         [TearDown]
@@ -59,6 +72,11 @@ namespace CodeYouApplyTests
             }
 
             return true;
+        }
+
+        private void NavigateToHomePage()
+        {
+            _driver.Navigate().GoToUrl(homepageUri);
         }
     }
 }
