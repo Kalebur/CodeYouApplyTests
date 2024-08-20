bd8697960ce2eccf98e979db5e00a68222b70799
jro12345 <josh.roehrig@waystar.com>
2024-08-20 14:32:45 -0400

    Update ApplicationPage.cs

--- CodeYouApplyTests/Selectors/ApplicationPage.cs
+++ CodeYouApplyTests/Selectors/ApplicationPage.cs
@@ -1,25 +1,39 @@
-﻿using NUnit.Framework.Internal.Execution;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using System.Numerics;
+using OpenQA.Selenium;
 
 namespace CodeYouApplyTests.Selectors
 {
-    public static class ApplicationPage
-    {
-        public static string Url { get; set; } = "https://code-you.org/apply/";
-        public static string SubmitButton { get; set; } = "//input[@id='submit_button']";
-        public static string StateDropdown { get; set; } = "(//select[@id='tfa_220'])[1]";
-        public static string BlankSubmissionErrorText { get; set; } = "The form is not complete and has not " +
-                "been submitted yet. There are 28 problems with your submission.";
+	public class ApplicationPage
+	{
+		private static IWebDriver _driver;
 
-        public static string FormIntroText { get; set; } = "//span[contains(text()," +
-            "'Please use this form to sign-up for Code:You. Appl')]";
+		public ApplicationPage(IWebDriver driver)
+		{
+			_driver = driver;
+		}
 
-        public static string ExpectedFormIntroText { get; set; } = "Please use this form to sign-up for Code:You. Applicants will be sent pre-work for the course on a rolling basis until spots are filled. Please note we have a considerable waiting list to get into the program – it could be many months (even a year or more) before your spot comes up in line.\r\n\r\nApplicants will be contacted to complete the necessary pre-work for the upcoming course a month or two before the class is slated to begin. Please check your email and be sure to whitelist info@code-you.org to receive periodic updates from our team.\r\n  \r\nIMPORTANT NOTE: Due to overwhelming interest in the program, we have a very long waiting list to get into the program. Sometimes a year or even more. Please be patient, but in the meantime you are welcome to contact info@code-you.org for any questions.";
+		// This should be named something more specific than Url...maybe something like applyPageUrl
+		public static string Url { get; set; } = "https://code-you.org/apply/";
 
-        public static List<string> ValidStateOptions { get; set; } = ["IN", "KY", "OH"];
-        public static string InvalidDateErrorText { get; set; } = "This does not appear to be a valid date.";
+		// Locators are usually written like this
+		public IWebElement submitButton => _driver.FindElement(By.XPath("//input[@id='submit_button']"));
+		// then you do not have to invoke a find element method in your test class
+		public static string SubmitButton { get; set; } = "//input[@id='submit_button']";
 
-        public static string Form { get; set; } = "//div[@id='tfa_515']";
-    }
+		public static string StateDropdown { get; set; } = "(//select[@id='tfa_220'])[1]";
+
+		// Maybe make this a string method that accepts a parameter for the number of errors present in the page
+		// This will only be valid if you have exactly 28 errors on the page and that number can change depending on what you leave blank in the form
+		public static string BlankSubmissionErrorText { get; set; } = "The form is not complete and has not " +
+						"been submitted yet. There are 28 problems with your submission.";
+
+		public static string FormIntroText { get; set; } = "//span[contains(text()," +
+				"'Please use this form to sign-up for Code:You. Appl')]";
+
+		public static string ExpectedFormIntroText { get; set; } = "Please use this form to sign-up for Code:You. Applicants will be sent pre-work for the course on a rolling basis until spots are filled. Please note we have a considerable waiting list to get into the program – it could be many months (even a year or more) before your spot comes up in line.\r\n\r\nApplicants will be contacted to complete the necessary pre-work for the upcoming course a month or two before the class is slated to begin. Please check your email and be sure to whitelist info@code-you.org to receive periodic updates from our team.\r\n  \r\nIMPORTANT NOTE: Due to overwhelming interest in the program, we have a very long waiting list to get into the program. Sometimes a year or even more. Please be patient, but in the meantime you are welcome to contact info@code-you.org for any questions.";
+
+		public static List<string> ValidStateOptions { get; set; } = ["IN", "KY", "OH"];
+		public static string InvalidDateErrorText { get; set; } = "This does not appear to be a valid date.";
+
+		public static string Form { get; set; } = "//div[@id='tfa_515']";
+	}
 }
